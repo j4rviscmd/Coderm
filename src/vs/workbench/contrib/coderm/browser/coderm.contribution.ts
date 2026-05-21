41e6862391e5e99e4ba053d73807794afe0a1e5b
j4rviscmd <j4rviscmd@gmail.com>
2026-05-21 21:32:11 +0900

    feat: auto-install Solarized Deep theme on first launch

--- src/vs/workbench/contrib/coderm/browser/coderm.contribution.ts
+++ src/vs/workbench/contrib/coderm/browser/coderm.contribution.ts
@@ -11,6 +11,7 @@
 import './activePaneBorder.js';
 import './autoMaximizeActions.js';
 import './cursorAutoHide.js';
+import './defaultThemeInstaller.js';
 import './devTitlePrefix.js';
 import './editorGroupIndex.js';
 import './preventNewGroupOnFocus.js';
